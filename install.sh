99f11cd452f9db9807d445719ebb17621df2168c
Stian R. Sporaland <Rodent1@users.noreply.github.com>
2023-07-07 14:40:21 +0200

    fix: change chezmoi install method

--- install.sh
+++ install.sh
@@ -10,8 +10,17 @@ echo "Installing build-essential..."
 sudo apt -y install build-essential &> /dev/null
 
 # Install Chezmoi
-echo "Installing chezmoi..."
-sh -c "$(curl -fsLS get.chezmoi.io)"
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
 
 # Echo out chezmoi command
 echo "Run the following command to apply dotfiles: chezmoi init --apply Rodent1"
