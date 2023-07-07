9ff8ef765bd06d927de6bae46bbe63ad33bf2da2
Stian R. Sporaland <Rodent1@users.noreply.github.com>
2023-07-07 15:14:15 +0200

    fix: revert to previous version

--- install.sh
+++ install.sh
@@ -9,6 +9,18 @@ sudo apt -y upgrade &> /dev/null
 echo "Installing build-essential..."
 sudo apt -y install build-essential &> /dev/null
 
-# Install chezmoi and init this repo
-echo "Installing chezmoi..."
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply Rodent1
+# Install Chezmoi
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
+
+# Echo out chezmoi command
+echo "Run the following command to apply dotfiles: chezmoi init --apply Rodent1"
