496a3ac21d7483420c01d039211c51a0e86f2d4b
Stian R. Sporaland <Rodent1@users.noreply.github.com>
2023-07-07 14:15:28 +0200

    fix: fix install.sh fish process

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ sudo apt -y upgrade &> /dev/null
 echo "Installing build-essential..."
 sudo apt -y install build-essential &> /dev/null
 
+# Install fish
+echo "Installing fish shell..."
+sudo apt -y install fish &> /dev/null
+chsh -s /usr/bin/fish
+fish
+
 # Install chezmoi and init this repo
 echo "Installing chezmoi..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply Rodent1
